1b82977496cee6466d3c63676b220805d9e04515
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-10-10 10:55:44 +0100

    getServerSideProps function config

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { client } from '../lib/client';
+
 import { Product, FooterBanner, HeroBanner  } from '../components'
 
-const Home = () => {
+const Home = ( { products, bannerData} ) => {
 return (
-    <>
+    <div>
       <HeroBanner />
 
       <div className='products-heading'>
@@ -16,8 +18,19 @@ return (
         {['Product 1', 'Product 2'].map((product)=> product)}
       </div>
       <FooterBanner />
-    </>
-  )
+    </div>
+  );
 }
 
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
 export default Home;
