89d98c053388999c3b1732273bc639fb243b4b45
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-10-10 10:28:49 +0100

    HeroBanner Structure

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { Product, FooterBanner, HeroBanner  } from '../components'
 const Home = () => {
 return (
     <>
-      HeroBanner
+      <HeroBanner />
 
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
@@ -15,7 +15,7 @@ return (
       <div className='products-container'>
         {['Product 1', 'Product 2'].map((product)=> product)}
       </div>
-      Footer
+      <FooterBanner />
     </>
   )
 }
